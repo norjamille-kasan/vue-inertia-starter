d9cb3b330e460e11523370f7e33de5bfa0ee1bd5
Jam <jamclong0328@gmail.com>
2024-08-31 10:13:09 +0800

    sync

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link ref="icon" href="{{ asset('images/zap.png') }}">
+        <link rel="icon" href="{{ asset('images/favicon.ico') }}" type="image/x-icon">
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
